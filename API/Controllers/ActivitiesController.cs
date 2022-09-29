bd8ae7636ae13f0b4cdf362f9de0552875aa8daa
Cercado, Gohan (Taguig City) <Gohan.Cercado@willistowerswatson.com>
2022-09-29 18:16:11 +0800

    thin controllers

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -23,15 +23,29 @@ namespace API.Controllers
         [HttpGet]
         public async Task<ActionResult<List<Activity>>> GetActivities()
         {
-            return await _mediator.Send(new List.Query());
+            return await Mediator.Send(new List.Query());
         }
 
         [HttpGet("{id}")] // /activities/id
         public async Task<ActionResult<Activity>> GetActivity(Guid id)
         {
             // return await _context.Activities.FindAsync(id);
-            return Ok();
+            return await Mediator.Send(new Details.Query{Id = id});
         }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateActivity(Activity activity) 
+        {
+            return Ok(await Mediator.Send(new Create.Command {Activity = activity}));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+        {
+            activity.Id = id;
+            return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+        }
+
 
     }
 }
